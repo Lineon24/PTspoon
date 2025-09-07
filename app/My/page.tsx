61b97500b4f40b3e120eec621c80c601349a12fd
Lineon24 <dlwnsgml2224@naver.com>
2025-09-07 23:02:57 +0900

    버튼 수정 및 상단바 내용 추가

--- app/My/page.tsx
+++ app/My/page.tsx
@@ -297,7 +297,7 @@ export default function ProfilePage() {
 
   return (
     <div className="w-full max-w-[540px] mx-auto flex flex-col h-[calc(100vh-128px)]"> {/* 메인 컨테이너 */}
-      <HeaderWithBack title={``} backTF={true} /> {/* 뒤로가기 헤더 */}
+      <HeaderWithBack title={`내 정보`} backTF={true} /> {/* 뒤로가기 헤더 */}
       <section className="bg-blue-100 p-10 text-center"> {/* 프로필 영역 */}
         <div className="flex justify-center gap-2 text-xl">
           {["😀", "🐮", "🐱", "🐸", "🐻"].map((emoji) => ( // 이모지 나열
